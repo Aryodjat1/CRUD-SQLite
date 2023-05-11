a599ca655ad2d9fa1dd3d847ab15a22c3fdb0ce8
AryoDjat1 <aryodjati5@gmail.com>
2023-05-11 20:56:37 +0700

    add function for button add

--- app/src/main/java/com/djatscode/crudsqlite/MainActivity.kt
+++ app/src/main/java/com/djatscode/crudsqlite/MainActivity.kt
@@ -2,13 +2,28 @@ package com.djatscode.crudsqlite
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.Data
+import android.widget.Toast
+import com.djatscode.crudsqlite.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: MainActivity
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnAdd.setOnClickListener {
+            val db = DatabaseHandler(this)
+            val user = User(
+                name = binding.etName.text.toString(),
+                phoneNum = binding.etNohp.text.toString(),
+                email = binding.etAddress.text.toString()
+            )
+            db.insertUser(user)
+            Toast.makeText(this, "Berhasil Menambahkan ke dalam DB", Toast.LENGTH_LONG).show()
+        }
     }
 }
